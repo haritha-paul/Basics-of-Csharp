173dc3d2589a0e6b352cb782cb0a0e57b236885c
Haritha Paul <harithapaul97@gmail.com>
2019-05-08 00:24:29 +0530

    Input and Output functions

---
+++ Read_Write/Mod1_lab1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Mod1_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //initializing with a default value
+            string firstname = "";
+            string lastname = "";
+            int age = 0;
+            string street = "";
+            string country = "";
+            DateTime birthdate;
+
+            //assigning some values
+
+            firstname = "susan";
+            lastname = "elizabeth";
+            age = 21;
+            street = "east india street";
+            country = "russia";
+            birthdate = new DateTime(1997, 1, 23);
+
+            //output to console
+
+            Console.Write(firstname);
+            Console.Write(lastname);
+
+            //using placeholder style
+
+            Console.Write("{0} years old", age);
+            Console.Write($"Born on {birthdate}");
+
+            //using string interpolation
+
+            Console.Write(street + "," + country);
+
+
+
+         
+
+        }
+    }
+}
