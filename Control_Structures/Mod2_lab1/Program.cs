28a116cdfa62f9aaa7400ce6d306f34ed29b69e7
Haritha Paul <harithapaul97@gmail.com>
2019-05-08 00:25:13 +0530

    Conditions and Iterations

---
+++ Control_Structures/Mod2_lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mod2_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Trying out if condition
+            Console.WriteLine("Please enter a value:");
+
+            int input = Int32.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("Even number...!!");
+            }
+            else
+            {
+                Console.WriteLine("Odd number..!!");
+            }
+
+            //Trying out loops
+
+            int n = input;
+            while(n > 0)
+            {
+                Console.WriteLine($"Current value of n is {n}");
+                n--;
+            }
+;        }
+    }
+}
